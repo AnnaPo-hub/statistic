140fc73afc0d5e3328e5dcabd6f7ec2f3670bf82
Anna Popova <anna.popova201909@gmail.com>
2020-03-10 14:08:24 +0300

    added jococo

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -7,15 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
     @Test
     void findMax() {
-        StatisticsService service=new StatisticsService();
-        long[] incomesInBillions = {12,5, 8, 4, 5, 3, 8, 6, 11, 11, 12,7,6,1};
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 7, 6, 1};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
 
     }
-
-
 
 
 }
